a3c88afcf357fba54d7fa20d9e3fc116bd01458c
Shailza Chaudhry <shailzachaudhry@gmail.com>
2025-05-22 09:43:39 +1000

    Terraform code

--- terraform/modules/ec2_instance/main.tf
+++ terraform/modules/ec2_instance/main.tf
@@ -1,91 +1,97 @@
-data "aws_ami" "amazon_linux_2023" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+  data "aws_ami" "amazon_linux_2023" {
+    most_recent = true
+    owners      = ["amazon"]
+    filter {
+      name   = "name"
+      values = ["al2023-ami-*-x86_64"]
+    }
   }
-}
-
-resource "aws_launch_template" "lt" {
-  name_prefix   = "${var.asg_name}-lt-"
-  image_id      = data.aws_ami.amazon_linux_2023.id
-  instance_type = "t3.micro"
-
-  iam_instance_profile {
-    name = var.instance_profile_name
+  data "aws_vpc" "default" {
+    default = true
   }
-
-  network_interfaces {
-    associate_public_ip_address = false
-    security_groups             = [aws_security_group.nginx_sg.id]
+  data "aws_subnets" "private" {
+    filter {
+      name   = "vpc-id"
+      values = [data.aws_vpc.default.id]
+    }
   }
+  resource "aws_iam_role" "ec2_role" {
+    name = "${var.asg_name}-ec2-role"
 
-  user_data = base64encode(templatefile("./userdata.sh.tpl", {
-    cw_agent_config = base64encode(file("./cloudwatch-config.json"))
-  }))
-}
-
-resource "aws_security_group" "nginx_sg" {
-  name_prefix = "${var.asg_name}-sg-"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    assume_role_policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "ec2.amazonaws.com"
+          }
+        }
+      ]
+    })
+  }
+  resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
+    role       = aws_iam_role.ec2_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_autoscaling_group" "asg" {
-  name                      = var.asg_name
-  desired_capacity          = 1
-  max_size                  = 1
-  min_size                  = 1
-  vpc_zone_identifier       = var.private_subnet_ids
-  health_check_type         = "EC2"
-  force_delete              = true
-  launch_template {
-    id      = aws_launch_template.lt.id
-    version = "$Latest"
+  resource "aws_iam_instance_profile" "ec2_profile" {
+    name = "${var.asg_name}-ec2-profile"
+    role = aws_iam_role.ec2_role.name
   }
 
-  target_group_arns = [var.load_balancer_target_group_arn]
+  resource "aws_launch_template" "lt" {
+    name_prefix   = "${var.asg_name}-lt-"
+    image_id      = data.aws_ami.amazon_linux_2023.id
+    instance_type = "t3.micro"
 
-  tag {
-    key                 = "Name"
-    value               = var.asg_name
-    propagate_at_launch = true
+    iam_instance_profile {
+      name = aws_iam_instance_profile.ec2_profile.name
+    }
+
+
+    user_data = base64encode(templatefile("./userdata.sh.tpl", {
+      cw_agent_config = base64encode(file("./cloudwatch-config.json"))
+    }))
   }
 
-  lifecycle {
-    create_before_destroy = true
+  resource "aws_security_group" "nginx_sg" {
+    name_prefix = "${var.asg_name}-sg-"
+    vpc_id      = var.vpc_id
+
+    ingress {
+      description = "Allow HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["10.0.0.0/8"]
+    }
+
+    egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
-  scheduled_action {
-    name                    = "${var.asg_name}-recycle"
-    recurrence              = "0 0 */30 * *"
-    min_size                = 1
-    max_size                = 1
-    desired_capacity        = 0
-    time_zone               = "UTC"
+  resource "aws_autoscaling_group" "asg" {
+    name                      = var.asg_name
+    desired_capacity          = 1
+    max_size                  = 1
+    min_size                  = 1
+    vpc_zone_identifier = data.aws_subnets.private.ids
+    vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+    max_instance_lifetime = 2592000
+    health_check_type         = "EC2"
+    force_delete              = true
+    launch_template {
+      id      = aws_launch_template.lt.id
+      version = "$Latest"
+    }
+    target_group_arns = [var.load_balancer_target_group_arn]
+
+
   }
 
-  scheduled_action {
-    name                    = "${var.asg_name}-recycle-restart"
-    recurrence              = "10 0 */30 * *"
-    min_size                = 1
-    max_size                = 1
-    desired_capacity        = 1
-    time_zone               = "UTC"
-  }
-}
